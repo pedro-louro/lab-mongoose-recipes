7178ffada89476f3b97b1845bad3d84be11a80b1
Pedro Louro <pedrolouro93@gmail.com>
2023-04-15 16:42:12 +0100

    Completed Lab

--- index.js
+++ index.js
@@ -5,19 +5,52 @@ const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
 const data = require('./data');
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = 'mongodb+srv://admin:admin@cluster0.nqnrgia.mongodb.net/recipe-app';
 
-// Connection to the database "recipe-app"
-mongoose
-  .connect(MONGODB_URI)
-  .then(x => {
-    console.log(`Connected to the database: "${x.connection.name}"`);
-    // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
-  })
-  .then(() => {
-    // Run your code here, after you have insured that the connection was made
-  })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
-  });
+async function updateDatabase() {
+  // Connection to the database "recipe-app"
+  try {
+    const x = await mongoose.connect(MONGODB_URI);
+    console.log("connected to", x.connections[0].name);
+    await Recipe.deleteMany();
+    
+    //Start coding here
+    //Iterarion 2 
+    await Recipe.create({
+      title: "Sardinha Assada",
+      level: "Easy Peasy",
+      ingredients: ["Sardinha", "batatas", "salada"],
+      cuisine: "mediterranean",
+      dishType: "main_course",
+      duration: 30,
+      creator: "Chef Manel",
+    })
+    const findRecipe = await Recipe.find({title: "Sardinha Assada"})
+    console.log(findRecipe[0].title)
+
+    // Iteration 3
+    await Recipe.insertMany(data)
+    const getRecepies = await Recipe.find()
+    getRecepies.forEach(recipe => {
+      console.log(recipe.title)
+    })
+
+    // Iteration 4
+    const UpdateOne = await Recipe.findOneAndUpdate({title: "Rigatoni alla Genovese"}, {duration: 100})
+    console.log(`Success! Duration updated to 100`)
+
+    // Iteration 5
+    await Recipe.deleteOne({title: "Carrot Cake"});
+    console.log(`Success! Carrot Cake deleted!`)    
+
+
+  } catch (e) {
+    console.error("Error connecting to the database", e);
+  }
+  finally{
+    mongoose.connection.close();
+  }
+}
+
+updateDatabase();
+
